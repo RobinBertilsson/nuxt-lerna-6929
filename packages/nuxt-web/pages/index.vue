2d7208f330b267fc1dfa356bfa86f64156229ee7
Robin Bertilsson <robin.bertilsson@capcito.com>
2020-06-22 17:11:25 +0200

    Initial commit

---
+++ packages/nuxt-web/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>Hello from index!!!!</div>
+    <MyButton />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { MyButton } from '@org/vue-scl'
+
+export default Vue.extend({
+  components: {
+    MyButton
+  }
+})
+</script>
---
+++ packages/vue-scl/src/components/MyButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Hello from MyButton
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'MyButton'
+})
+</script>
